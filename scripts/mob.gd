c299ccc5511f8771c2686d94642ffcb0e140c082
KauanHK <kauankaestner06@gmail.com>
2025-10-18 16:36:21 -0300

    interação com o mob

--- scripts/mob.gd
+++ scripts/mob.gd
@@ -1,7 +1,17 @@
 extends Area2D
 
+var is_player_in: bool = false
+
 signal responder_pergunta
 
 
+func _process(delta: float) -> void:
+	if is_player_in and Input.is_action_just_pressed("interact"):
+		responder_pergunta.emit(self)
+
 func _on_body_entered(body: Node2D) -> void:
-	responder_pergunta.emit(self)
+	is_player_in = true
+
+
+func _on_body_exited(body: Node2D) -> void:
+	is_player_in = false
